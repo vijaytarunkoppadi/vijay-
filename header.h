f9e9d39c39bcdd97db32dac7eeb1e0f09c37ee7f
vijaytarunkoppadi <92502371+vijaytarunkoppadi@users.noreply.github.com>
2023-12-24 23:08:22 +0530

    Update header.h

--- header.h
+++ header.h
@@ -6,14 +6,6 @@ typedef signed int s32;
 extern void delay_sec(unsigned int sec);
 extern void delay_ms(unsigned int ms);
 
-extern void lcd_data(unsigned char data);
-extern void lcd_cmd(unsigned char cmd);
-extern void lcd_init(void);
-extern void lcd_string(char *ptr);
-extern void rtol(char *,int pos);
-extern void ltor(char *,int pos);
-extern void cgram_init(void);
-
 extern void uart0_init(unsigned int baud);
 extern void uart0_tx(unsigned char data);
 extern void uart0_tx_string(s8 *ptr);
@@ -24,11 +16,6 @@ extern void uart0_tx_float(float n);
 extern int uart0_rx_integer(s8 *p);
 extern void uart0_tx_hex(int num);
 
-extern void adc_init(void);
-extern u32 adc_read(u32 ch_num);
 
-extern void config_VIC(void);
-extern void en_uart0_intr(void);
-extern void UART0_Handler(void)__irq;
 
 
